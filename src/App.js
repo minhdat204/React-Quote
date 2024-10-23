7c39302b168cb3ad76c1d1fc87fab8bcd073de49
Dat <0306221011@caothang.edu.vn>
2024-10-24 01:19:22 +0700

    component_Add_Advice

--- src/App.js
+++ src/App.js
@@ -96,7 +96,6 @@ function App() {
     const value = inputAddAdvice.value;
     addAdvice(value);
     inputAddAdvice.value = '';
-    setOpen(false);
   }
   //theme
   const [ mode, setMode ] = useState(true);
@@ -113,74 +112,7 @@ function App() {
     setMode((prev) => !prev);
   };
   //modal
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 600,
-    bgcolor: 'background.paper',
-    border: '0px solid #000',
-    boxShadow: 24,
-    p: 4,
-    borderRadius: '15px',
-  };
-  //input textarea
-  const blue = {
-    100: '#DAECFF',
-    200: '#b6daff',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
-    900: '#003A75',
-  };
-  
-  const grey = {
-    50: '#F3F6F9',
-    100: '#E5EAF2',
-    200: '#DAE2ED',
-    300: '#C7D0DD',
-    400: '#B0B8C4',
-    500: '#9DA8B7',
-    600: '#6B7A90',
-    700: '#434D5B',
-    800: '#303740',
-    900: '#1C2025',
-  };
-  
-  const TextareaAutosize = styled(BaseTextareaAutosize)(
-    ({ theme }) => `
-    box-sizing: border-box;
-    width: 100%;
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: 8px 12px;
-    border-radius: 8px;
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  
-    &:hover {
-      border-color: ${blue[400]};
-    }
-  
-    &:focus {
-      border-color: ${blue[400]};
-      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
-    }
-  
-    // firefox
-    &:focus-visible {
-      outline: 0;
-    }
-  `,
-  );
+
   return (
     <Box sx={{position: 'absolute', left: '10%', right: '10%'}}>
       <ThemeProvider theme={theme}>
@@ -220,28 +152,13 @@ function App() {
             <IconButton onClick={() => toggleMode()} aria-label="theme change" title='Theme change'>
               { mode? <DarkModeIcon /> : <LightModeIcon />}
             </IconButton>
-            <IconButton onClick={handleOpen} aria-label="add advice" title='Add advice'>
-              <AddIcon fontSize='medium'/>
-            </IconButton>
+            
+            <ModalAddAdvice handleAddAdvice={handleAddAdvice}/>
+            
             <IconButton aria-label="share" title='Share'>
               <FormatListBulletedIcon />
             </IconButton>
-            <Modal
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box sx={modalStyle}>
-                <Typography id="modal-modal-title" variant="h6" component="h1" marginBottom={'20px'} textAlign={'center'} gutterBottom>
-                  Add new advice
-                </Typography>
-                <TextareaAutosize id='inputAdd' aria-label="empty textarea" placeholder="Text here" fullWidth/>
-                <Box display = 'flex' justifyContent={'flex-end'} marginTop={'5px'}>
-                  <Button onClick={() => handleAddAdvice()} variant="contained" size='medium' sx={{borderRadius: '8px', fontWeight: '600'}}>Add</Button>
-                </Box>
-              </Box>
-            </Modal>
+
           </CardActions>
           </Card>
         </Grid>
